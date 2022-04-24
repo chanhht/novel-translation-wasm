128952ca7a73520e429bc14e25f6acb31e8da967
Chanh Hua <chanh.hua@magnolia-cms.com>
2022-04-24 11:16:31 +0700

    init setup

---
+++ src/lib.rs
@@ -0,0 +1,52 @@
+use wasm_bindgen::prelude::*;
+// use std::fs;
+// use aho_corasick::AhoCorasick;
+// use futures::executor;
+use wasm_bindgen_futures::JsFuture;
+use wasm_bindgen::JsCast;
+use web_sys::{Request, RequestInit, RequestMode, Response};
+
+#[wasm_bindgen]
+extern "C" {
+    pub fn alert(s: &str);
+    // pub async fn loadVietphrase() -> JsValue;
+}
+
+#[wasm_bindgen]
+pub async fn convert() -> String {
+    // alert(&format!("Hello, {}!", name));
+    let vietphrases = load_vietphrase().await;
+    // let vietphrases = executor::block_on(loadVietphrase().await?);
+    println!("Finish wait");
+    match vietphrases {
+        Ok(v) => return format!("Hello, {}!", v.as_string().unwrap()),
+        Err(_v) => return "Error!".to_string(),
+    }
+}
+
+async fn load_vietphrase() -> Result<JsValue, JsValue> {
+    let mut opts = RequestInit::new();
+    opts.method("GET");
+    opts.mode(RequestMode::Cors);
+
+    let url = "dicts/vietphrase-test.txt";
+
+    let request = Request::new_with_str_and_init(&url, &opts)?;
+
+    request
+        .headers()
+        .set("Accept", "application/text")?;
+
+    let window = web_sys::window().unwrap();
+    let resp_value = JsFuture::from(window.fetch_with_request(&request)).await?;
+
+    // `resp_value` is a `Response` object.
+    assert!(resp_value.is_instance_of::<Response>());
+    let resp: Response = resp_value.dyn_into().unwrap();
+
+    // Convert this other `Promise` into a rust `Future`.
+    let text = JsFuture::from(resp.text()?).await?;
+
+    Ok(text)
+
+}
